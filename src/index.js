60ce52e8d9af5da701cea310029a9cd8677ced14
Omer Kahoot <omer@mauqa.online>
2020-06-24 03:35:48 +0500

    adding redux dev tools extension code to debug redux store states and jumping between states usings its chrome extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 //import reducer from './store/reducer';
 
@@ -27,8 +27,10 @@ const logger = store => {
     }
 };
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 //const store = createStore(reducer);
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
