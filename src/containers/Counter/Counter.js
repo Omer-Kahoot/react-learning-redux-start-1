8b3bcfe60a1a5fb031ca4690c01e69c63c98f095
Omer Kahoot <omer@mauqa.online>
2020-06-24 01:09:56 +0500

    for react redux immutable working on arrays and passing action payloads to redux stores

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,26 +5,6 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
 
     render () {
         return (
@@ -32,8 +12,15 @@ class Counter extends Component {
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddValue}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractValue}  />
+                <CounterControl label="Add 5" clicked={() => this.props.onAddValue(5)}  />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractValue(5)}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(sr=>(
+                        <li key={sr.id} onClick={() =>this.props.onDeleteResult(sr.id)}>{sr.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -41,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -52,6 +40,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter : () => dispatch({type: 'DECREMENT'}),
         onAddValue : (value) => dispatch({type: 'ADD', value: value}),
         onSubtractValue : (value) => dispatch({type: 'SUBTRACT', value: value}),
+        onStoreResult : () => dispatch({type: 'STORE'}),
+        onDeleteResult : (id) => dispatch({type: 'DELETE', resultElementId : id})
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,28 +1,31 @@
 const initialState = {
-    counter : 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + action.value
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
-    }
+    switch (action.type) {
+        case 'INCREMENT':
+            const newState = Object.assign ({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+        case 'DECREMENT':
+            return { ...state, counter: state.counter - 1 }
+        case 'ADD':
+            return { ...state, counter: state.counter + action.value }
+        case 'SUBTRACT':
+            return { ...state, counter: state.counter - action.value }
+        case 'STORE' :
+            return { ...state, results : [...state.results, {id: new Date(), value: state.counter}]}
+        case 'DELETE':
+            //One way of deleting from an array immutably. return {...state, results: [...state.results].splice(action.value, 1)}\
+            //const updatedArray = state.results.filter((el, index) => index !== action.value);
+            const updatedArray = state.results.filter(el => action.resultElementId !== el.id);
+            return {
+                ...state,
+                results : updatedArray
+            }
+    }    
 
     return state;
 }
